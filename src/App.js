6c3f05a0c2644d59ba519ebe888556e9d67d23bc
mitaksim <miriamt.simonnet@gmail.com>
2020-01-29 20:52:06 +0100

    Get the user's email and stock it in a state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import api from './services/api';
 import './App.css';
 
@@ -6,10 +6,23 @@ import './App.css';
 import logo from './Assets/coffee-logo2.png';
 
 function App() {
+  // Stocker une information dans le state
+  // email = valeur de l'input
+  // setEmail = actualise la valeur de l'email
+  const [email, setEmail] = useState('');
+
   // Création de la méthode qui va nous permettre d'écouter l'événement sur le bouton
   function handleSubmit(event) {
     event.preventDefault();
-    console.log('Hello');
+
+    console.log(email);
+
+    }
+
+    // evénement qui change la valeur de l'email
+    function handleEmailChange(event) {
+      setEmail(event.target.value);
+    
   }
   return (
     <div className="container">
@@ -24,6 +37,10 @@ function App() {
           type="email" 
           id="email" 
           placeholder="Votre e-mail" 
+          value={email}
+          // A chaque fois que l'user change la valeur de l'email (onChange) on reçoit un event
+          //onChange={event => setEmail(event.target.value)}
+          onChange={handleEmailChange}
           />
 
           <button className="btn" type="submit">Validez</button>
